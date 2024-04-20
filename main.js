9a691226b73d13621d10dc9392089ea9a1bc2803
CipiVlad <supercoder.cipi@gmail.com>
2024-04-20 15:48:43 +0200

    added onclick logic for burger menu

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
+const hamburger = document.getElementById('hamburger')
+let menu = document.getElementById('menu')
 
+window.onload = () => {
+    menu.style.display = 'none'
+}
 
+hamburger.addEventListener('click', () => {
+
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+
+})
